2baadf24d0918f4743c95fec1d02ad7f562c0976
eduard <webeduard@yandex.ru>
2018-09-20 20:28:30 +0300

    implemented initialization function of the game

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,6 @@
 var scores, roundScore, activePlayer;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
-
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = 0;
-document.getElementById('score-1').textContent = 0;
-document.getElementById('current-0').textContent = 0;
-document.getElementById('current-1').textContent = 0;
+init();
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
   // 1.Random number
@@ -65,4 +56,29 @@ function nextPlayer() {
   document.querySelector('.player-1-panel').classList.toggle('active');
 
   document.querySelector('.dice').style.display = 'none';
+}
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init() {
+  scores = [0, 0];
+  roundScore = 0;
+  activePlayer = 0;
+
+  document.querySelector('.dice').style.display = 'none';
+
+  document.getElementById('score-0').textContent = 0;
+  document.getElementById('score-1').textContent = 0;
+  document.getElementById('current-0').textContent = 0;
+  document.getElementById('current-1').textContent = 0;
+
+  document.getElementById('name-0').textContent = 'игрок 1';
+  document.getElementById('name-1').textContent = 'игрок 2';
+
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+
+  document
+    .querySelector('.player-' + activePlayer + '-panel')
+    .classList.add('active');
 }
